293f03ce9cbc7e068b9c42ee83888963720ccd95
ahalim150 <ahmedabdelhalim150@gmail.com>
2024-06-21 14:18:54 +0300

    feat: implement the debounce technique with the search input

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let resultsContainer = document.getElementsByClassName("container")[0]
+let resultsContainer = document.getElementsByClassName("container")[0];
+let debounceTimeout;
+
+const debounce = (func, delay) => {
+    return (...args) => {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+};
 
 const validateInput = (el) => {
-    if(el.value === ""){
-        resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-    }else{
-        generateResults(el.value, el)
+    if (el.value === "") {
+        resultsContainer.innerHTML = "<p>Type something in the above search input</p>";
+    } else {
+        debounceGenerateResults(el.value, el);
     }
-}
+};
 
 const generateResults = (searchValue, inputField) => {
     fetch(
@@ -15,23 +25,30 @@ const generateResults = (searchValue, inputField) => {
     )
     .then(response => response.json())
     .then(data => {
-        let results = data.query.search
-        let numberOfResults = data.query.search.length
-        resultsContainer.innerHTML = ""
-        for(let i=0; i<numberOfResults; i++) {
-            let result = document.createElement("div")
-            result.classList.add("results")
+        let results = data.query.search;
+        let numberOfResults = data.query.search.length;
+        resultsContainer.innerHTML = "";
+        for (let i = 0; i < numberOfResults; i++) {
+            let result = document.createElement("div");
+            result.classList.add("results");
             result.innerHTML = `
             <div>
                 <h3>${results[i].title}</h3>
                 <p>${results[i].snippet}</p>
             </div>
             <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
-            `
-            resultsContainer.appendChild(result)
+            `;
+            resultsContainer.appendChild(result);
         }
-        if(inputField.value === ""){
-            resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
+        if (inputField.value === "") {
+            resultsContainer.innerHTML = "<p>Type something in the above search input</p>";
         }
-    })
-}
+    });
+};
+
+const debounceGenerateResults = debounce(generateResults, 300);
+
+// Attach the validateInput function to the input element
+document.querySelector('.search input').addEventListener('keyup', function() {
+    validateInput(this);
+});
